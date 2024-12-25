1a68083ce66b3c0cc7d9b9c3beefe518582c89b8
oula-1rddw <melel4338@gmail.com>
2024-12-25 21:11:06 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("attendButton").addEventListener("click", function () {
+    alert("Thank you for confirming your attendance! 🎉");
+  });
+  
+  document.getElementById("notAttendButton").addEventListener("click", function () {
+    alert("We’re sorry you can’t make it. 😢");
+  });
+  
